803b64399afca938972f5d44e9c0b521920df51b
rennehan <douglas.rennehan@gmail.com>
2024-08-21 09:04:47 -0400

    Keeping track of previous and next values in the cell.

--- src/Cell/Cell.hpp
+++ src/Cell/Cell.hpp
@@ -9,10 +9,15 @@
 class Cell
 {
 private:
-    std::shared_ptr<std::vector<box_int>> coordinates;
-    std::shared_ptr<std::vector<real>> velocity;
-    real density;
-    real energy;
+    std::shared_ptr<std::vector<box_int>> prev_coordinates;
+    std::shared_ptr<std::vector<real>> prev_velocity;
+    real prev_density;
+    real prev_energy;
+
+    std::shared_ptr<std::vector<box_int>> next_coordinates;
+    std::shared_ptr<std::vector<real>> next_velocity;
+    real next_density;
+    real next_energy;
 
 public:
     Cell(const std::vector<box_int> &init_coordinates, 
@@ -20,34 +25,43 @@ public:
          const real init_density,
          const real init_energy)
     {
-        coordinates = std::make_shared<std::vector<box_int>>(init_coordinates);
-        velocity = std::make_shared<std::vector<real>>(init_velocity);
-        density = init_density;
-        energy = init_energy;
+        prev_coordinates = std::make_shared<std::vector<box_int>>(init_coordinates);
+        prev_velocity = std::make_shared<std::vector<real>>(init_velocity);
+        prev_density = init_density;
+        prev_energy = init_energy;
+
+        next_coordinates = std::make_shared<std::vector<box_int>>();
+        next_velocity = std::make_shared<std::vector<real>>();
+        for (uint8_t d = 0; d < DIMENSION; d++) {
+            next_coordinates->push_back(0);
+            next_velocity->push_back(0.);
+        }
+        next_density = 0.;
+        next_energy = 0.;
     };
 
     std::shared_ptr<std::vector<box_int>> get_coordinates() const {
-        return coordinates;
+        return prev_coordinates;
     }
 
-    box_int get_coordinates(uint8_t dimension) const {
-        return (*coordinates)[dimension];
+    box_int get_coordinates(const uint8_t dimension) const {
+        return (*prev_coordinates)[dimension];
     }
 
     real get_density() const {
-        return density;
+        return prev_density;
     }
 
     real get_energy() const {
-        return energy;
+        return prev_energy;
     }
 
     std::shared_ptr<std::vector<real>> get_velocity() const {
-        return velocity;
+        return prev_velocity;
     }
 
     real get_velocity(uint8_t index) const {
-        return (*velocity)[index];
+        return (*prev_velocity)[index];
     }
 };
 
